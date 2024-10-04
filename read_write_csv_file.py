cd926bfe3e95871fb6e95bf21b0bbc3ff922a56e
ALOKDUBEYFORU <DubeyAlok@rediffmail.com>
2024-10-04 21:49:39 +0530

    added try except finally block in the read_write_csv_file.py

--- read_write_csv_file.py
+++ read_write_csv_file.py
@@ -1,8 +1,15 @@
 import csv
-with open(r'C:\temp\test.csv') as f:
-    readfile = csv.DictReader(f)
-    for line in readfile:
-        with open(r'C:\temp\test1.csv','w') as f1:
+try:
+    with open(r'C:\temp\test.csv') as f:
+        readfile = csv.DictReader(f)
+        for line in readfile:
+            with open(r'C:\temp\test1.csv','w') as f1:
 
-            writefile = csv.DictWriter(f)
-            writefile.writerow(line['ï»¿Company Name'])
+                writefile = csv.DictWriter(f)
+                writefile.writerow(line['ï»¿Company Name'])
+
+except Exception as e:
+    print(e)
+
+finally:
+    print('Program ended')
